5c737aed816cbd2f502e99d05a8341909cdcc70e
Jafeth-Gonzalez <jprosnew@gmail.com>
2024-11-01 17:31:36 -0500

    Create code_class.py
    codigo de la clase 1

---
+++ code_class.py
@@ -0,0 +1,16 @@
+def get_significate(word , dictionary):
+    if word in dictionary.keys():
+        return dictionary[word]# devolver el significado de la palabra 
+    else:
+        return "No se encuentra en el diccionario , lo siento" # que le devuelvo ¿? no hay significado para su palabra
+#el diccionario fue hecho por chatgpt , a mi me dio pereza 
+slang_diccionario = {
+    "LOL": "una respuesta a algo gracioso",
+    "CRINGE": "algo raro o embarazoso",
+    "ROFL": "una respuesta a una broma",
+    "SHEESH": "ligera desaprobación",
+    "CREEPY": "aterrador, siniestro",
+    "AGGRO": "ponerse agresivo/enojado"
+}
+significate = get_significate(input("Escribe una palabra que no entiendas (¡con mayúsculas!): "),slang_diccionario) #me es mas facil asi
+print(significate)#no se que comentar ya que decir  que decir "devuelve  el significado" es muy obvio  y no se como dar contexto
